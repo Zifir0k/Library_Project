366d7ffca3dbb39bc84e213e76b6d3ba888a36c1
Zifir0k <evgenizef.ak@gmail.com>
2024-02-02 09:48:20 +1000

    First step

---
+++ src/main/java/killjoy/models/Book.java
@@ -0,0 +1,57 @@
+package killjoy.models;
+
+public class Book {
+    private int id;
+    private int personId;
+    private String name;
+    private String author;
+    private int ear;
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getEar() {
+        return ear;
+    }
+
+    public void setEar(int ear) {
+        this.ear = ear;
+    }
+
+    public Book(int id, int personId, String name, String author, int ear) {
+        this.id = id;
+        this.personId = personId;
+        this.name = name;
+        this.author = author;
+        this.ear = ear;
+    }
+}
---
+++ src/main/java/killjoy/models/Person.java
@@ -0,0 +1,57 @@
+package killjoy.models;
+
+public class Person {
+    private int id;
+    private String name;
+    private String secondName;
+    private String surname;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person(int id, String name, String secondName, String surname, int age) {
+        this.id = id;
+        this.name = name;
+        this.secondName = secondName;
+        this.surname = surname;
+        this.age = age;
+    }
+}
